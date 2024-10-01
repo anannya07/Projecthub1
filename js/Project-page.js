c70e25e41862c792b7be0951d82388b845e7e7f7
Arpan110204 <arpanmukherjee211@gmail.com>
2024-10-01 16:18:44 +0530

    Updated all files

--- js/Project-page.js
+++ js/Project-page.js
@@ -2,15 +2,41 @@
 document.getElementById('clickable-card').addEventListener('click', function() {
     window.location.href = 'ai-ml.html'; // Redirect to the new page
   });
-  
+  document.getElementById('clickable-card2').addEventListener('click', function() {
+    window.location.href = 'data_science.html'; // Redirect to the new page
+  });
+  document.getElementById('clickable-card3').addEventListener('click', function() {
+    window.location.href = 'cyber_security.html'; // Redirect to the new page
+  });
+  document.getElementById('clickable-card4').addEventListener('click', function() {
+    window.location.href = 'web3.html'; // Redirect to the new page
+  });
+  document.getElementById('clickable-card5').addEventListener('click', function() {
+    window.location.href = 'IoT.html'; // Redirect to the new page
+  });
+  document.getElementById('clickable-card6').addEventListener('click', function() {
+    window.location.href = 'home_automation.html'; // Redirect to the new page
+  });
+  document.getElementById('clickable-card7').addEventListener('click', function() {
+    window.location.href = 'ar_vr.html'; // Redirect to the new page
+  });
+  document.getElementById('clickable-card8').addEventListener('click', function() {
+    window.location.href = 'finance.html'; // Redirect to the new page
+  });
+  document.getElementById('clickable-card9').addEventListener('click', function() {
+    window.location.href = 'health_care.html'; // Redirect to the new page
+  });
+
+
+
   /*******batch page hrefs*********** */
   // Define an object with batch IDs as keys and their corresponding links
 const batchLinks = {
   "batch-2024": "Batch-2024.html",
-  "batch-2023": "link-for-2023.html",
-  "batch-2022": "link-for-2022.html",
-  "batch-2021": "link-for-2021.html",
-  "batch-2020": "link-for-2020.html"
+  "batch-2023": "Batch-2023.html",
+  "batch-2022": "Batch-2022.html",
+  "batch-2021": "Batch-2021.html",
+  "batch-2020": "Batch-2020.html"
 };
 
 // Iterate through each entry in the object
@@ -38,12 +64,12 @@ Object.keys(batchLinks).forEach(batchId => {
 // Define an object with card IDs as keys and their corresponding links
 const cardLinks = {
   "card-cse": "Dept-CSE.html",
-  "card-csbs": "csbs.html",
-  "card-ece": "ece.html",
-  "card-eee": "eee.html",
-  "card-ee": "ee.html",
-  "card-me": "me.html",
-  "card-mca": "mca.html"
+  "card-csbs": "Dept-CSBS.html",
+  "card-ece": "Dept-ECE.html",
+  "card-eee": "Dept-EEE.html",
+  "card-ee": "Dept-EE.html",
+  "card-me": "Dept-ME.html",
+  "card-mca": "Dept-MCA.html"
 };
 
 // Iterate through each entry in the object
